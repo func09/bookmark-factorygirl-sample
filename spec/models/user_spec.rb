bbdb9f0a7767cd0a22da2c1c0e76d2c3b9a92a98
func09 <mitsuru.haga@gmail.com>
2009-04-26 16:19:20 +0900

    Userの関連が正しく設定されていることを保証するスペックを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,16 @@ describe User do
     @user = Factory.build(:bob)
     @user.save.should be_true
   end
+
+  it "ボブはブックマークを２つ持っていること" do
+    @bob = Factory(:bob)
+    @bob.should have(2).bookmarks
+  end
+
+  it "マイクはブックマークを２つ持っていること" do
+    @mike = Factory(:mike)
+    @mike.should have(1).bookmarks
+  end
+  
   
 end
